0adccc49646b052a7d993c6ac75487a3274632b6
Joe Bacal <bacalj@gmail.com>
2017-11-30 16:00:55 -0500

    actually a bg image instead

--- catgrid.php
+++ catgrid.php
@@ -41,7 +41,8 @@ function catgrid_taxonomy_grid($atts, $content = null) {
     foreach ($catgridos as $cat) {
         echo '<article class="catgrid-article ' . $tax . '"><a href="'. $cat['term_link'] .'">';
             echo '<div class="catgrid-img-wrap">';
-                echo '<img class="catgrid-img" src="' . $cat['img_url'] . '">';
+                //echo '<img class="catgrid-img" src="' . $cat['img_url'] . '">';
+                echo '<div class="catgrid-bg-img" style="background-image:url(' . $cat['img_url'] . ')"></div>';
             echo '</div>';
             echo '<div class="catgrid-cat-title">';
                 echo $cat['name'];
